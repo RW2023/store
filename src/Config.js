d60d7f1025e929848968972067d8f660a4605c66
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-04-21 20:27:07 -0400

    Update firebase configuration to use environment variables

--- src/Config.js
+++ src/Config.js
@@ -1,19 +1,17 @@
-// Import the functions you need from the SDKs you need
+// src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
-
-const firebaseConfig = {
-  apiKey: "AIzaSyABiGna9pSuelixJ31mVMD-UDICnyTfUoQ",
-  authDomain: "store-56343.firebaseapp.com",
-  databaseURL: "https://store-56343-default-rtdb.firebaseio.com",
-  projectId: "store-56343",
-  storageBucket: "store-56343.appspot.com",
-  messagingSenderId: "229281471965",
-  appId: "1:229281471965:web:5383e56cd5bd43348a2a34",
-  measurementId: "G-NYQXRDGZJ8"
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const imageDb = getStorage(app);
